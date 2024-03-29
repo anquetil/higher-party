32115dae3d7ae7932c82398cec5170d50a746a50
Martin <25070051+anquetil@users.noreply.github.com>
2024-03-29 14:48:04 +0100

    change from defillam to uniswap

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation'
 
 
 export default async function Swap() {
-  redirect('https://swap.defillama.com/?chain=base&from=0x0000000000000000000000000000000000000000&to=0x0578d8A44db98B23BF096A382e016e29a5Ce0ffe')
+  redirect('https://app.uniswap.org/swap?chain=base&inputCurrency=ETH&outputCurrency=0x0578d8A44db98B23BF096A382e016e29a5Ce0ffe')
 }
